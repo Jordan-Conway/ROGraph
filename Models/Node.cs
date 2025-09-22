e872b3ecf77dd5ae6df3bb700171a79441faa1c3
Jordan-Conway <jordan_conway@outlook.com>
2025-09-22 20:53:27 +0100

    Add coordiantes to Node class

--- Models/Node.cs
+++ Models/Node.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 
 namespace ROGraph.Models
 {
-    public class Node
+    public class Node(string name, NodeType type, int x = 0, int y = 0)
     {
-        public required string Name { get; set; }
-        public required NodeType Type { get; set; }
+        public required string Name { get; set; } = name;
 
-        public List<Node> Children { get; set; } = new List<Node>();
+        public required int X { get; set; } = x;
+
+        public required int Y { get; set; } = y;
+        public required NodeType Type { get; set; } = type;
     }
 }
