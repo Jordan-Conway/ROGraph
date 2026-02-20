64732e4321da300f63ef8641e7b7098afd25cb4e
Jordan Conway <jordan_conway@outlook.com>
2026-02-20 22:17:03 +0000

    Remove old scripts

--- Backend/src/DataProviders/SQLiteProviders/Scripts/GetAllNodesForReadingOrder.sql
+++
@@ -1,6 +0,0 @@
--- Ideally this would be a view or function but I couldn't work out how to do that with a parameter
-SELECT * 
-FROM nodes n
-JOIN nodes_readingOrders nr
-ON n.id = nr.nodeId
-WHERE nr.nodeId = @id;
