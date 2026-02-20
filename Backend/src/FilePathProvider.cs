3a6dc92fed81909bd7c39e94229a175192bdac16
Jordan Conway <jordan_conway@outlook.com>
2026-02-20 22:13:42 +0000

    Update FilePathProvider

---
+++ Backend/src/FilePathProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace ROGraph.Backend;
+
+internal static class FilePathProvider
+{
+    private const string ConfigFileName = "config.json";
+    private const string DbFileName = "data.db";
+    private static string _appDataDirectory = "";
+
+    public static string GetConfigFilePath()
+    {
+        SetAppDataDirectory();
+        return Path.Combine(_appDataDirectory, ConfigFileName);
+    }
+
+    public static string GetDatabaseFilePath()
+    {
+        SetAppDataDirectory();
+        return Path.Combine(_appDataDirectory, DbFileName);
+    }
+
+    private static void SetAppDataDirectory()
+    {
+        _appDataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ROGraph");
+    }
+}
