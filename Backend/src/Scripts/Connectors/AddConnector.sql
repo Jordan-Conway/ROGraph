3a57e8355a0a2c07246a375cc84efde17e8adf35
Jordan Conway <jordan_conway@outlook.com>
2026-02-22 14:28:40 +0000

    Fix connectors not saving

--- Backend/src/Scripts/Connectors/AddConnector.sql
+++ Backend/src/Scripts/Connectors/AddConnector.sql
@@ -1,3 +1,3 @@
 INSERT INTO Connectors (x1, y1, x2, y2, readingOrderId)
 VALUES (@x1, @y1, @x2, @y2, @roId)
-ON CONFLICT (@x1, @y1, @x2, @y2, @roId) DO NOTHING;
+ON CONFLICT (x1, y1, x2, y2, readingOrderId) DO NOTHING;
